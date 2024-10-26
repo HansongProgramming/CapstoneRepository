edc9cd843f1d7c269a1b9322b389e48c8e2c61d9
Hansongli <125562851+HansongProgramming@users.noreply.github.com>
2024-10-26 13:39:29 +0800

    fixing stuff

--- Simulator/blender_config/build_config.py
+++
@@ -1,6 +0,0 @@
-# Build configuration for Blender source code
-# Customize which modules are enabled or disabled
-
-ENABLE_PARTICLES = True
-ENABLE_SIMULATION = True
-ENABLE_RENDERING = False  # Disable rendering if you only need physics
